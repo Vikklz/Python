1810f70ed0b54efad35aed73876388aa571917fc
Vikklz <36885045+Vikklz@users.noreply.github.com>
2021-04-13 16:16:45 +0300

    Add files via upload

---
+++ KinelVV_DZ2_N1.py
@@ -0,0 +1,8 @@
+firs_expr = 15 * 3
+print(type(firs_expr))
+second_expr = 15 / 3
+print(type(second_expr))
+third_expr = 15 // 2
+print(type(third_expr))
+fourth_expr = 15 ** 2
+print(type(fourth_expr))
